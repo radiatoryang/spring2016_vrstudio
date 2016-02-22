679d2ccafd70cf69bb703b1494336515c991fa82
Robert Yang <yang.robert.w@gmail.com>
2016-02-22 18:11:46 -0500

    week04: added simple MouseLook.cs and RaycastTrigger.cs

---
+++ week04/Assets/scripts/MouseLook.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// GetAxis returns a number between -1 and 1
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+
+		// rotate the camera
+		transform.Rotate( -mouseY * Time.deltaTime * 180f, mouseX * Time.deltaTime * 180f, 0f );
+
+		// corrective pass: unroll the camera
+		transform.localEulerAngles = new Vector3( transform.localEulerAngles.x,
+												  transform.localEulerAngles.y,
+			                                      0f );
+	}
+}
---
+++ week04/Assets/scripts/RaycastTrigger.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastTrigger : MonoBehaviour {
+
+	bool wasLooking = false; // used for OnStartLook and OnEndLook
+	
+	// Update is called once per frame
+	void Update () {
+		// generate the ray before we raycast it
+		Ray ray = new Ray( Camera.main.transform.position,
+						   Camera.main.transform.forward );
+		// this var will tell us where and what it hit
+		RaycastHit rayHitInfo = new RaycastHit();
+
+		Debug.DrawRay( ray.origin, ray.direction * 1000f, Color.yellow );
+	
+		// actually shooting the raycast now
+		if ( Physics.Raycast( ray, out rayHitInfo, 1000f ) 
+			 && rayHitInfo.transform == this.transform ) {
+			// is the raycast hitting the thing we put this script on?
+			OnLooking();
+			if ( wasLooking == false ) {
+				wasLooking = true;
+				OnStartLook();
+			}
+		} else {
+			OnNotLooking();
+			if ( wasLooking == true ) {
+				wasLooking = false;
+				OnEndLook();
+			}
+		}
+
+	}
+
+	// code that will happen when I start looking at something
+	void OnStartLook () {
+
+	}
+
+	// code that will happen the instant when I stopped looking at something
+	void OnEndLook () {
+
+	}
+
+	// code that will happen CONSTANTLY as long as we're looking at it
+	void OnLooking () {
+		transform.Rotate(0f, 15f, 0f);
+		// turn red over time as long as we keep looking at it
+		GetComponent<Renderer>().material.color = Color.Lerp( GetComponent<Renderer>().material.color,
+															  Color.red,
+															  Time.deltaTime * 0.5f);
+	}
+
+	// code that will happen CONSTANTLY when we are NOT looking at it
+	void OnNotLooking () {
+		transform.localScale *= 1.001f; // get bigger 101% of current size
+	}
+
+
+
+}
