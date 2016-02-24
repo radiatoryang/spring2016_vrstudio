4d1cedf87bc949ca78bafc986f91330567da259f
Robert Yang <yang.robert.w@gmail.com>
2016-02-24 16:17:54 -0500

    week04b: RaycastPaint.cs (moving spheres, cloning spheres)

---
+++ week04/Assets/scripts/RaycastPaint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaycastPaint : MonoBehaviour {
+
+	public Transform sphere; // assign in inspector
+	
+	// Update is called once per frame
+	void Update () {
+		// setting up our ray
+		Ray ray = new Ray( Camera.main.transform.position,
+						   Camera.main.transform.forward );
+		// initializing a variable for later
+		RaycastHit rayHitInfo = new RaycastHit();
+
+		// actually shoot the raycast now
+		if ( Physics.Raycast( ray, out rayHitInfo, 1000f ) ) {
+			// move the sphere to where the raycast hit the cube
+			Debug.DrawRay( ray.origin, ray.direction * rayHitInfo.distance, Color.yellow );
+			// sphere.position = rayHitInfo.point; // point = world position of impact
+			Instantiate( sphere, rayHitInfo.point, Quaternion.identity );
+		}
+	}
+}
---
+++ week04/Assets/scripts/VRUtility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.VR;
+
+public class VRUtility : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false; // hides the cursor
+		Cursor.lockState = CursorLockMode.Locked; // lock the cursor in middle of screen
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKeyDown(KeyCode.RightBracket) ) { // increase visual quality
+			VRSettings.renderScale = Mathf.Clamp01( VRSettings.renderScale + 0.1f);
+		}
+		if ( Input.GetKeyDown(KeyCode.LeftBracket) ) { // decrease visual quality
+			VRSettings.renderScale = Mathf.Clamp01( VRSettings.renderScale - 0.1f);
+		}
+
+		if ( Input.GetKeyDown(KeyCode.R) ) { // reset VR HMD's orientation
+			InputTracking.Recenter();
+		}
+	}
+}
