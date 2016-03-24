10803e4fc209194145662e5beaff2d77686804eb
Robert Yang <yang.robert.w@gmail.com>
2016-03-23 22:22:03 -0400

    week08: mecanim code stuff -- IKDemo.cs, LookAtDemo.cs

---
+++ week08/Assets/scripts/IKDemo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IKDemo : MonoBehaviour {
+
+	Animator animator;
+	public Transform ikTarget; // the "goal" to solve towards
+	public AvatarIKGoal ikType; // LeftHand? RightHand? LeftFoot? RightFoot?
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	void OnAnimatorIK () { // all IK functions must go inside OnAnimatorIK
+		animator.SetIKPosition( ikType, ikTarget.position );
+		animator.SetIKPositionWeight( ikType, 1f );
+		animator.SetIKRotation( ikType, ikTarget.rotation );
+		animator.SetIKRotationWeight( ikType, 1f );
+	}
+
+}
---
+++ week08/Assets/scripts/LookAtDemo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtDemo : MonoBehaviour {
+
+	Animator animator;
+	public Transform lookTarget;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void OnAnimatorIK () {
+		// visualizing what SHOULD be happening
+		Debug.DrawLine( animator.GetBoneTransform( HumanBodyBones.Head ).position,
+						lookTarget.position,
+						Color.yellow );
+		// set the look target
+		animator.SetLookAtPosition( lookTarget.position );
+		animator.SetLookAtWeight( 1f, 1f );
+	}
+}
