f693dd1d0fd84a2093809d5e2c65e365e891e59f
Robert Yang <yang.robert.w@gmail.com>
2016-03-30 17:34:54 -0400

    week09: navMeshAgent workflow demo, animation layer (DanceLayerDemo.cs), choreographing action with coroutines (Director.cs)

---
+++ week09/Assets/scripts/DanceLayerDemo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DanceLayerDemo : MonoBehaviour {
+
+	Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// press [SPACE] to control dancing layer
+		// make sure the dancing layer is set to "additive"
+		if ( Input.GetKey(KeyCode.Space) ) {
+			// set DanceLayer weight to 1
+			myAnimator.SetLayerWeight(1, 1f);
+		} else {
+			// set DanceLayer weight to 0
+			myAnimator.SetLayerWeight(1, 0f);
+		}
+	}
+}
---
+++ week09/Assets/scripts/Director.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Director : MonoBehaviour {
+
+	public Transform npcMoveTarget; // assign in Inspector
+	public NavMeshAgent npcAgent; // assign in Inspector
+
+	// Use this for initialization
+	void Start () {
+		// MyFirstCoroutine(); // DON'T DO THIS
+
+		// you must always start a coroutine using StartCoroutine
+		StartCoroutine( MyFirstCoroutine() );
+	}
+		
+	// a Coroutine is a special kind of function that can
+	// last more than 1 frame + we can control how fast it runs
+	IEnumerator MyFirstCoroutine () {
+		Debug.Log("the coroutine started!");
+		yield return 0; // pause for a frame
+		Debug.Log("ok, I waited for 1 frame, now continuing...");
+		yield return 0;
+		yield return 0; // waited for 2 frames
+		Debug.Log("ok I waited 2 frames now!");
+
+		// sometimes it's easier to wait for seconds, vs. counting frames
+		yield return new WaitForSeconds(1.6f);
+		Debug.Log("ok I waited for 1.6 seconds!");
+
+		// move the NPC move target (a sphere? cylinder?) to control NPC
+		npcMoveTarget.position = new Vector3( 0f, 0f, 2f );
+		yield return new WaitForSeconds(5f);
+		npcMoveTarget.position = new Vector3( 5f, 0f, -2f );
+
+		// wait until I press SPACE before continuing...
+		while ( !Input.GetKey(KeyCode.Space) ) {
+			yield return 0;
+		}
+		// after we press space, the coroutine will continue...
+
+		// we can also tell Unity to yield for the duration of a coroutine
+		yield return StartCoroutine( WaitUntilDestination( Vector3.zero ) );
+
+		// the coroutine is now finished, and will end
+		Debug.Log("cutscene finished!");
+	}
+
+	IEnumerator WaitUntilDestination ( Vector3 newDestination ) {
+		npcMoveTarget.position = newDestination;
+
+		yield return new WaitForSeconds(2f); // thank you, terry
+
+		// this code will keep waiting as long as Remaining Distance > 1.0f
+		while ( npcAgent.remainingDistance > 1f ) {
+			yield return 0;
+		}
+	}
+
+
+}
